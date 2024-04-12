fcb51fa0ce47cada82de1f166edd1c1e9b1bbdb6
Artyom Novikov <lemerl12@mail.ru>
2024-04-12 11:37:29 +0300

    Create a project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='test'>
+      <p className='test__p'>test</p>
+      </div>
+    </>
+  )
+}
+
+export default App
