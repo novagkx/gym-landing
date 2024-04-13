f93400c9d9e39d5cd3d71822336d075768b7f856
Artyom Novikov <lemerl12@mail.ru>
2024-04-14 00:34:01 +0300

    Create AdvantagesSection and remake header

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import AdvantagesSection from './components/AdvantagesSection'
 function App() {
 
 
   return (
     <>
      <Header/>
+     <AdvantagesSection/>
     </>
   )
 }
