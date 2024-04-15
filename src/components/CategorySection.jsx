d4f3ed4eea3f00b5615f3a6c5e01c5c60e81a1f1
Artyom Novikov <lemerl12@mail.ru>
2024-04-15 23:20:19 +0300

    Update styles, create productSection

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -1,44 +1,48 @@
+import HeadersBlock from "./HeadersBlock";
+
 export default function CategorySection() {
   return (
     <section className="category-section">
       <div className="category-section__container">
-        <div className="category-section__info-container">
-          <h2 className="category-section__main-header">category</h2>
-          <a href="#" className="category-section__link">See all</a>
-        </div>
+        <HeadersBlock blockName={"category-section"} title={"category"}/>
         <div className="category-section__list-container">
           <ul className="category-section__list">
             <li className="category-section__item">
+            <a href="#">
               <h3 className="category-section__header">Protein & Nutrition</h3>
               <img
                 className="category-section__img"
                 src="src/images/Protein.svg"
                 alt=""
               />
+              </a>
             </li>
             <li className="category-section__item">
-              <h3 className="category-section__header">Sports Outfit</h3>
+            <a href="#"><h3 className="category-section__header">Sports Outfit</h3>
               <img
                 className="category-section__img"
                 src="src/images/outfit.svg"
                 alt=""
               />
+              </a>
             </li>
             <li className="category-section__item">
-              <h3 className="category-section__header">Sports Bags</h3>
+            <a href="#"><h3 className="category-section__header">Sports Bags</h3>
               <img
                 className="category-section__img"
                 src="src/images/bags.svg"
                 alt=""
               />
+              </a>
             </li>
             <li className="category-section__item">
-              <h3 className="category-section__header">GYM Accessories</h3>
+            <a href="#"><h3 className="category-section__header">GYM Accessories</h3>
               <img
                 className="category-section__img"
                 src="src/images/accessories.svg"
                 alt=""
               />
+              </a>
             </li>
           </ul>
         </div>
